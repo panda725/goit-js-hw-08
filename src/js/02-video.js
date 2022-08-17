4d5f084f66b589470e835e97851178e0fe55086d
Alex Supotnytskyi <kedred725@gmail.com>
2022-08-17 11:13:56 -0400

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+const videoPlayerRef = document.querySelector('#vimeo-player');
+console.log(videoPlayerRef);
+
+const player = new Player(videoPlayerRef);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
