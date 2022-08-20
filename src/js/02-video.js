e42494278c85d486ce10fa62f122d23280dd6a43
Alex Supotnytskyi <kedred725@gmail.com>
2022-08-20 14:09:11 -0400

    end with task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,30 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const videoPlayerRef = document.querySelector('#vimeo-player');
-console.log(videoPlayerRef);
+const LOCAL_KEY = 'videoplayer-current-time';
 
 const player = new Player(videoPlayerRef);
 
-player.on('play', function () {
-  console.log('played the video!');
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(LOCAL_KEY, e.seconds);
+  }),
+  1000
+);
+
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY)).catch(function (error) {
+  switch (error.name) {
+    // что нужно было указывать в case ?
+    // setCurrentTime(seconds: number): Promise<number, (RangeError|Error)>
+    case 'RangeError':
+      localStorage.getItem(LOCAL_KEY) < 0;
+      // the time was less than 0 or greater than the video’s duration
+      break;
+
+    default:
+      alert('Invalid time duration');
+      break;
+  }
 });
