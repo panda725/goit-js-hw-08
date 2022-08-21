61b208269b25fea2f736d1e8eddd50b4bf506c06
Alex Supotnytskyi <kedred725@gmail.com>
2022-08-21 03:18:15 -0400

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,17 +14,4 @@ player.on(
   1000
 );
 
-player.setCurrentTime(localStorage.getItem(LOCAL_KEY)).catch(function (error) {
-  switch (error.name) {
-    // что нужно было указывать в case ?
-    // setCurrentTime(seconds: number): Promise<number, (RangeError|Error)>
-    case 'RangeError':
-      localStorage.getItem(LOCAL_KEY) < 0;
-      // the time was less than 0 or greater than the video’s duration
-      break;
-
-    default:
-      alert('Invalid time duration');
-      break;
-  }
-});
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY));
