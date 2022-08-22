d7a61c442795bb98fa63bdb508890bfbe5a442d7
Alex Supotnytskyi <kedred725@gmail.com>
2022-08-22 07:40:17 -0400

    ended with task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const parseJson = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener(
+  'input',
+  throttle(evt => {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+  }, 500)
+);
+
+getValueFromLocalstorage();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log('натякніть як вивести?)', 'email:', 'message:');
+}
+
+function getValueFromLocalstorage() {
+  const savedText = parseJson;
+  if (savedText) {
+    const values = Object.entries(savedText);
+    values.forEach(function ([name, value]) {
+      refs.form.elements[name].value = value;
+    });
+  }
+}
