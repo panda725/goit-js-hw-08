e256c5863f0d63d588687c5cb80d599bfc60a4fc
Alex Supotnytskyi <kedred725@gmail.com>
2022-08-23 11:00:22 -0400

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,7 +6,7 @@ const refs = {
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const parseJson = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-const formData = {};
+const formData = { ...parseJson };
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener(
@@ -21,9 +21,13 @@ getValueFromLocalstorage();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  evt.currentTarget.reset();
+  const inputsData = new FormData(evt.currentTarget);
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  console.log('натякніть як вивести?)', 'email:', 'message:');
+  evt.currentTarget.reset();
+
+  inputsData.forEach((name, value) => {
+    console.log(value, `=>`, name);
+  });
 }
 
 function getValueFromLocalstorage() {
